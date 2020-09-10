6fb89c02474eb93cb5cade72c480ac5d433157d7
Johan Solbakken <johsol@stud.ntnu.no>
2020-09-10 18:00:30 +0200

    Updates

---
+++ precache-manifest.93b238bfec96669c4edf9f725336b60d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec4d9c120d6bdfc7243e82dcd361f86",
+    "url": "/Infoskjerm-kurs/index.html"
+  },
+  {
+    "revision": "1114c44393905dc5f5ed",
+    "url": "/Infoskjerm-kurs/static/css/main.c0282195.chunk.css"
+  },
+  {
+    "revision": "a2a6989c72208078f01d",
+    "url": "/Infoskjerm-kurs/static/js/2.734a2fd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Infoskjerm-kurs/static/js/2.734a2fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1114c44393905dc5f5ed",
+    "url": "/Infoskjerm-kurs/static/js/main.9842e22f.chunk.js"
+  },
+  {
+    "revision": "5c8d156ed3dfa15609e0",
+    "url": "/Infoskjerm-kurs/static/js/runtime-main.424a4c8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Infoskjerm-kurs/static/media/logo.5d5d9eef.svg"
+  }
+]);
